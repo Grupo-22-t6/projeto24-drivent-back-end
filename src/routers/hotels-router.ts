0204611c1332f0883894825c9533ecdf61130606
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-11-02 17:19:35 -0300

    feat: get rooms vaccancies route

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,4 +1,4 @@
-import { getHotelInfo } from '@/controllers';
+import { getHotelInfo, getRoomsVacancies } from '@/controllers';
 import { authenticateToken, validateBody } from '@/middlewares';
 import { Router } from 'express';
 
@@ -7,5 +7,6 @@ const hotelsRouter = Router();
 ///hotelsRouter.all('/*', authenticateToken);
 
 hotelsRouter.get('/', getHotelInfo);
+hotelsRouter.get('/rooms', getRoomsVacancies);
 
 export { hotelsRouter };
