726f02c9a64354bd3df65e4e09b563387fa7dfc8
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-11-02 17:20:25 -0300

    feat: get rooms vaccancies repository

--- src/repositories/hotels-repository/index.ts
+++ src/repositories/hotels-repository/index.ts
@@ -1,11 +1,60 @@
 import { prisma } from '@/config';
 
 async function getHotels() {
-  return prisma.hotel.findMany();
+  return await prisma.hotel.findMany();
+}
+
+async function getRoomsVacancies() {
+  const firstHotelRooms = await prisma.room.groupBy({
+    by: ['accommodationType'],
+    where: {
+      hotelId: {
+        equals: 1,
+      },
+    },
+    _sum: {
+      accommodationType: true,
+    },
+  });
+
+  const secondHotelRooms = await prisma.room.groupBy({
+    by: ['accommodationType'],
+
+    where: {
+      hotelId: {
+        equals: 2,
+      },
+    },
+
+    _sum: {
+      accommodationType: true,
+    },
+  });
+
+  const thirdHotelRooms = await prisma.room.groupBy({
+    by: ['accommodationType'],
+
+    where: {
+      hotelId: {
+        equals: 3,
+      },
+    },
+
+    _sum: {
+      accommodationType: true,
+    },
+  });
+
+  return {
+    firstHotelRooms,
+    secondHotelRooms,
+    thirdHotelRooms,
+  };
 }
 
 const hotelsRepository = {
   getHotels,
+  getRoomsVacancies,
 };
 
 export default hotelsRepository;
