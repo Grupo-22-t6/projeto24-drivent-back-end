e92e38e1fbf8b4db7b9feefc71269a691d1ba59a
Renan Araujo <renanaraujo852@hotmail.com>
2022-11-05 23:22:54 -0300

    feat: router and controller of reserve room

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,3 +1,4 @@
+import { AuthenticatedRequest } from '@/middlewares';
 import hotelsService from '@/services/hotels-service';
 import { Response, Request } from 'express';
 import httpStatus from 'http-status';
@@ -12,4 +13,9 @@ export async function getRoomsByHotel(req: Request, res: Response) {
   const hotelId = Number(req.params.id);
   const rooms = await hotelsService.getRoomsByHotel(hotelId);
   res.status(httpStatus.OK).json(rooms);
+}
+
+export async function reserveRoom(req: AuthenticatedRequest, res: Response) {
+  const { userId } = req;
+  res.status(httpStatus.OK).json();
 }
