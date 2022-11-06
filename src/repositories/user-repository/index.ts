e3503d249eb1366a3ac1fb5cce511188b1193df5
Renan Araujo <renanaraujo852@hotmail.com>
2022-11-06 00:01:17 -0300

    feat: find by id user repository

--- src/repositories/user-repository/index.ts
+++ src/repositories/user-repository/index.ts
@@ -20,9 +20,19 @@ async function create(data: Prisma.UserUncheckedCreateInput) {
     data,
   });
 }
-
+async function findById(id: number) {
+  return prisma.user.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      reserve: true,
+    },
+  });
+}
 const userRepository = {
   findByEmail,
+  findById,
   create,
 };
 
